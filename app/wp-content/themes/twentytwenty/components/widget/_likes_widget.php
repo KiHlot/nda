b39040d50bcf3f4d87c0219de5215344cafdc584
kirill.glot <kirylhlot@gmail.com>
2026-07-30 01:34:49 +0300

    First commit

--- app/wp-content/themes/twentytwenty/components/widget/_likes_widget.php
+++ app/wp-content/themes/twentytwenty/components/widget/_likes_widget.php
@@ -9,13 +9,13 @@
     
     return '
       <div class="likes_widget" data-post-id="'.$post->ID.'">
-        <button class="button decrement" data-type="decrement" '.($type === '-1' ? 'disabled' : '' ).'>
+        <button class="button decrement" data-type="decrement" '.($type === '-1' ? 'disabled' : '' ).' aria-label="Decrement">
           <svg width="13" height="2" viewBox="0 0 13 2" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 1H1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         </button>
         <div class="count">'.$count.'</div>
-        <button class="button increment" data-type="increment" '.($type === '1' ? 'disabled' : '' ).'>
+        <button class="button increment" data-type="increment" '.($type === '1' ? 'disabled' : '' ).' aria-label="Increment">
           <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 0.34396 0.34396" fill="#ffffff">
             <path d="M0.19844 0.3175c0,0.01461 -0.01185,0.02646 -0.02646,0.02646 -0.01461,0 -0.02646,-0.01185 -0.02646,-0.02646l0 -0.11906 -0.11906 0c-0.01461,0 -0.02646,-0.01185 -0.02646,-0.02646 0,-0.01461 0.01185,-0.02646 0.02646,-0.02646l0.11906 0 0 -0.11906c0,-0.01461 0.01185,-0.02646 0.02646,-0.02646 0.01461,0 0.02646,0.01185 0.02646,0.02646l0 0.11906 0.11906 0c0.01461,0 0.02646,0.01185 0.02646,0.02646 0,0.01461 -0.01185,0.02646 -0.02646,0.02646l-0.11906 0 0 0.11906z"/>
           </svg>
--- app/wp-content/themes/twentytwenty/front-page.php
+++ app/wp-content/themes/twentytwenty/front-page.php
@@ -1,6 +1,6 @@
 <?php $page_id = get_the_ID(); ?>
 <?php get_header(); ?>
-  <div class="main_layout">
+  <main class="main_layout">
     <div class="main_wrapper">
       <div class="layout_content">
 	      <h1 class="layout_heading">Статьи</h1>
@@ -8,5 +8,5 @@
       </div>
       <h2 class="layout_sidebar">Sidebar</h2>
     </div>
-  </div>
+  </main>
 <?php get_footer(); ?>
